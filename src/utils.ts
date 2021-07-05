73b2b10c8d2a4fcef79f20f478d6f2262f21a37f
Sri Panyam <sri.panyam@gmail.com>
2021-07-05 01:11:00 -0700

    Adding common methods into utils

---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+export class GroupCounter {
+  value = -1;
+  next(): number {
+    return ++this.value;
+  }
+  get current(): number {
+    return this.value;
+  }
+}
+
+export function isSpace(ch: string): boolean {
+  return ch == " " || ch == "\t" || ch == "\n" || ch == "\r";
+}
