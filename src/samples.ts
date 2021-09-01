616d98c4c8dab358c61b261ac01c239d4b0dce46
Sri Panyam <sri.panyam@gmail.com>
2021-08-31 21:02:42 -0700

    Changing default regex re mode

--- src/samples.ts
+++ src/samples.ts
@@ -8,7 +8,7 @@ export const DOUBLE_QUOTE_STRING = (): Regex => flexRE`[']([^'\\\n]|\\.|\\\n)*['
 export const SIMPLE_JS_STRING = (): string => '"(.*?(?<!\\\\))"';
 
 // Disabled as Safari will fail this one
-export function JS_REGEXP(mode: "native" | "with_lb" | "without_lb" = "native") {
+export function JS_REGEXP(mode: "native" | "with_lb" | "without_lb" = "without_lb") {
   if (mode == "native") {
     if (!TSU.Browser.IS_SAFARI()) {
       return /\/(.+?(?<!\\))\/([imus]*)/;
