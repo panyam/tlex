e8b4dd42e7a3f451275b44dd08ebd5076df3b912
Sri Panyam <sri.panyam@gmail.com>
2022-12-21 18:15:11 -0800

    Cleanups

--- site/components/RulesView/index.tsx
+++ site/components/RulesView/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import defaultStyles from "./view.module.scss";
 import BaseComponent from "../BaseComponent";
-import * as events from "../events"
+import * as events from "../events";
 import { builtinLexers } from "./configs";
 import * as DSL from "../dsl";
-import { timeIt, stripLinePrefixSpaces  } from "../utils";
+import { timeIt, stripLinePrefixSpaces } from "../utils";
 
-export default class RulesView extends BaseComponent<{ styles?: any, }> {
+export default class RulesView extends BaseComponent<{ styles?: any }> {
   state: any;
   langSelectRef = React.createRef<HTMLSelectElement>();
   rulesTextareaRef = React.createRef<HTMLTextAreaElement>();
@@ -20,7 +20,7 @@ export default class RulesView extends BaseComponent<{ styles?: any, }> {
       modified: false,
     };
   }
-    
+
   get styles() {
     return this.props.styles || defaultStyles;
   }
@@ -28,27 +28,35 @@ export default class RulesView extends BaseComponent<{ styles?: any, }> {
   render() {
     const styles = this.styles;
     const state = this.state;
-    const lexerOptions = builtinLexers.map(bl => { return (
-      <option value={bl.name}
-              selected = {state.selectedLang == bl.name ||
-                          bl.selected || false}>{bl.label}</option>
-    )});
+    let selectedLang = "";
+    const lexerOptions = builtinLexers.map((bl, index) => {
+      if (bl.name == state.selectedLang || bl.selected) {
+        selectedLang = bl.name;
+      }
+      return <option key={index}>{bl.label}</option>;
+    });
     return (
-      <div className = {styles.rootView}>
+      <div className={styles.rootView}>
         <div className={styles.headingView}>
           <span>Enter Rules</span>
           <span>{state.modified ? "(modified)" : ""}</span>
         </div>
-        <textarea ref={this.rulesTextareaRef}
-                  onKeyDown={this.onInputChanged.bind(this)}
-                  className={styles.inputTextArea} />
+        <textarea
+          ref={this.rulesTextareaRef}
+          onKeyDown={this.onInputChanged.bind(this)}
+          className={styles.inputTextArea}
+        />
         <div className={styles.optionsArea}>
-            <span style={{float: "left"}} > Examples:</span>
-            <select style={{float: "left"}} ref={this.langSelectRef}
-                    onChange={this.onLangChanged.bind(this)}>
-              {lexerOptions}
-            </select>
-            <button style={{float: "right"}} >Compile</button>
+          <span style={{ float: "left" }}>Examples:</span>
+          <select
+            defaultValue={selectedLang}
+            style={{ float: "left" }}
+            ref={this.langSelectRef}
+            onChange={this.onLangChanged.bind(this)}
+          >
+            {lexerOptions}
+          </select>
+          <button style={{ float: "right" }}>Compile</button>
         </div>
       </div>
     );
